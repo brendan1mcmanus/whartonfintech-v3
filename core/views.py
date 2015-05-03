f8a4d780478bd8c2cf7af784200e82e19453c7b7
Sudipta Bandyopadhyay <sbandyopadhyay1@gmail.com>
2015-05-03 18:19:21 -0400

    Update home page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 # Create your views here.
 def home(request):
+  blog = Blog.objects.filter(published=True).latest('dateline')
   return render(request, 'pages/home.html', {
     'menu_context': ('home',),
+    'blog': blog,
     'is_homepage': True,
   })
