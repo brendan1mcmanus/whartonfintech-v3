183b6481445a176e60299e0941affb33ff4031f5
Brendan <brndnmcmns@gmail.com>
2015-10-12 17:38:22 -0400

    new directors added to email list

--- contact_request/models.py
+++ contact_request/models.py
@@ -25,13 +25,13 @@ class ContactRequest(models.Model):
   )
 
   TO_EMAILS = {
-     1: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>'],
-     2: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>'],
-     3: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>'],
+     1: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>', 'Neha Goel <nehagoel@wharton.upenn.edu>'],
+     2: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>', 'Neha Goel <nehagoel@wharton.upenn.edu>'],
+     3: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>', 'Neha Goel <nehagoel@wharton.upenn.edu>'],
      4: ['Richard Bhamb <bhamb@wharton.upenn.edu>'],
      5: ['Richard Bhamb <bhamb@wharton.upenn.edu>'],
      6: ['Daniel McAuley <dmcauley@wharton.upenn.edu>', 'Steve Weiner <weins@wharton.upenn.edu>'],
-     7: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>'],
+     7: ['Sasha Dobrolioubov <adobro@wharton.upenn.edu>', 'Matthew Applegate <apmatt@wharton.upenn.edu>'],
      8: ['Cameron Peake <cpeake@wharton.upenn.edu>'],
      9: ['Cameron Peake <cpeake@wharton.upenn.edu>'],
     10: ['Cameron Peake <cpeake@wharton.upenn.edu>'],
